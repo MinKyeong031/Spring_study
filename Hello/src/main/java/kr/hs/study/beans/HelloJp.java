cb8b7ddb8ba951455d867bdd3d1c46ebafca2fd3
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-04-20 07:59:06 +0900

    HelloPrj 과제

---
+++ Hello/src/main/java/kr/hs/study/beans/HelloJp.java
@@ -0,0 +1,15 @@
+package kr.hs.study.beans;
+
+public class HelloJp implements Hello{
+
+	public void bye1() {
+		System.out.println("사요나라1");
+	}
+	public void bye2() {
+		System.out.println("사요나라");
+	}
+	public void bye3() {
+		System.out.println("사요나라");
+	}
+
+}
---
+++ Hello/src/main/java/kr/hs/study/beans/HelloKr.java
@@ -0,0 +1,13 @@
+package kr.hs.study.beans;
+
+public class HelloKr implements Hello {
+	public void Hello1() {
+		System.out.println("¾È³ç1");
+	}
+	public void Hello2() {
+		System.out.println("¾È³ç2");
+	}
+	public void Hello3() {
+		System.out.println("¾È³ç3");
+	}
+}
